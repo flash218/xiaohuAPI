524605f6f7dd54eb0e2bc5471d87c3ecd121b99c
陈小龙 <13480731740@qq.com>
2017-02-21 20:35:48 +0800

    注册API完成

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,22 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//封装经常出现的方法函数
+function rq($key=null,$default=null){
+    if(!$key)
+        return Request::all();
+    return Request::get($key,$default);
+}
+
+function user_ins(){
+    return $user = new App\User();
+}
+
+Route::get('api',function () {
+    return ['version'=>'1.1'];
+});
+
+Route::post('api/user',function () {
+    return user_ins()->signup();
+});
--- app/User.php
+++ app/User.php
@@ -23,4 +23,45 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    // 用户注册API ***************************
+    public function signup()
+    {
+        $has_username_and_password = $this->has_username_and_password();
+
+        if (!$has_username_and_password)
+            return ['status'=>1,'msg'=>'用户名和密码皆不可为空'];
+        $username = $has_username_and_password[0];
+        $password = $has_username_and_password[1];
+
+        /*检查用户名是否存在*/
+        $user_exists = $this->where('username',$username)->exists();
+
+        if ($user_exists)
+            return ['status'=>0,'msg'=>'用户名已存在'];
+
+        /*加密密码*/
+        $hashed_password = bcrypt($password);
+
+        /*存入数据库*/
+        $user = $this;
+        $user->username = $username;
+        $user->password = $hashed_password;
+        if ($user->save())
+            return ['status'=>1,'id'=>$user->id,'msg'=>'用户注册成功！'];
+        else
+            return ['status'=>0,'msg'=>'用户注册失败！'];
+    }
+
+    //封装经常出现的方法函数
+    protected function has_username_and_password(){
+        $username = rq('username');
+        $password = rq('password');
+
+        /*检查用户名是否为空*/
+        if($username && $password)
+            return [$username,$password];
+        return false;
+    }
+
 }
